885e558a5e4455a0375cc7f7404c8695c1a244c1
Liam Sutton <devliam90@gmail.com>
2019-09-18 14:39:27 +0100

    add edit buttons to team members

--- team-builder/src/components/TeamMembers.js
+++ team-builder/src/components/TeamMembers.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledHeaders = styled.h4`
-  font-size: 1.4rem;
-  padding: 1rem 0;
-  margin: 0;
+const StyledHeaders = styled.div`
+  width: 40%;
+  margin: 1.6rem 0;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    h4 {
+      font-size: 1.4rem;
+      padding: 1rem 0;
+      margin: 0;
+    }
+    button {
+      padding: .5rem 1rem;
+      font-size: 1rem;
+      justify-content: center;
+      background: none;
+      border-radius: 6px;
+      border: 1px solid #e8e8e8;
+      cursor: pointer;
+      outline: none;
+
+      &:hover {
+        box-shadow: 1px 1px 1px #e8e8e8;
+      }
+    }
+  }
 `;
 
 const TeamMembers = ({ teamMembersData }) => {
   return (
-    <div>
+    <StyledHeaders>
       {teamMembersData.map(teamMember => (
-        <StyledHeaders key={teamMember.id}>
-          {teamMember.name} is a {teamMember.role}
-        </StyledHeaders>
+        <div>
+          <h4
+            key={teamMember.id}
+          >
+            {teamMember.name} is a {teamMember.role}
+          </h4>
+          <button>Edit</button>
+        </div>
       ))}
-    </div>
+    </StyledHeaders>
   );
 };
 
